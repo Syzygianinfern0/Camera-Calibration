cbf4924ea63f50cc68795ca2d8b7ced5f0b6a72d
Syzygianinfern0 <spsharan2000@gmail.com>
2019-08-26 21:30:15 +0530

    Trackbar for alpha

--- undistortion.py
+++ undistortion.py
@@ -1,20 +1,30 @@
 import numpy
 import cv2
 import pickle
-import glob
+
+
+def nothing(x):
+    pass
+
 
 with open("data.pickle", "rb") as handler:
     [objpoints, imgpoints] = pickle.load(handler)
 
-images = glob.glob('assets/*.jpg')
+img = cv2.imread('assets/left01.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.namedWindow('bars')
+cv2.createTrackbar('alpha', 'bars', 100, 100, nothing)
+
+while True:
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
     h, w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    alpha = cv2.getTrackbarPos('alpha', 'bars') / 100.0
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), alpha, (w, h))
 
     # undistort
     dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
@@ -22,10 +32,16 @@ for fname in images:
     # crop the image
     x, y, w, h = roi
     dst = dst[y:y + h, x:x + w]
-    file_name = 'outputs\\' + fname.split('\\')[1]
-    cv2.imwrite(file_name, dst)
+
     cv2.imshow('calibrated', dst)
     cv2.imshow('original', gray)
-    cv2.waitKey(500)
+
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        mean_error += error
+
+    print("total error: ", mean_error / len(objpoints))
 
 cv2.destroyAllWindows()
